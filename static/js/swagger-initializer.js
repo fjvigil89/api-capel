20fdbb2e6aff92ff3493e637038c855917826353
vreyes07 <vladimireyess@gmail.com>
2022-06-17 09:27:08 +0200

    Removed limits in querys. Documentation added. And Readme.md updated

--- static/js/swagger-initializer.js
+++ static/js/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "static/openapi.json",
+    url: "/static/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
